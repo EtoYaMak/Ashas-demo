1835c60f6126a067c4a062f2cf257d2cae365d53
Mak <itsmak98@gmail.com>
2023-08-09 21:41:49 +0100

    separate datafile

--- components/Menu.jsx
+++ components/Menu.jsx
@@ -1,83 +1,9 @@
 "use client";
 
-import MenuItem from "../app/menu/MenuItem/page";
+import MenuItem from "@app/menu/MenuItem/page";
+import { menuItems } from "@public/menuItems"; // adjust the path based on where you saved the file
 
 const Menu = ({ onVideoSelected }) => {
-  const menuItems = [
-    {
-      title: "Chicken Biryani",
-      imgSrc: "/assets/images/ChickenBiryani.jpg",
-      price: "£19",
-      videoUrl: "/assets/vid.mp4",
-    },
-    {
-      title: "Chicken Biryani",
-      imgSrc: "/assets/images/ChickenDhabaCurry.jpg",
-      price: "£19",
-      videoUrl: "/assets/vid.mp4",
-    },
-    {
-      title: "Chicken Biryani",
-      imgSrc: "/assets/images/MuscatGosht2.jpg",
-      price: "£19",
-      videoUrl: "/assets/vid.mp4",
-    },
-    {
-      title: "Chicken Biryani",
-      imgSrc: "/assets/images/PresidentPlatter.jpg",
-      price: "£19",
-      videoUrl: "/assets/vid.mp4",
-    },
-    {
-      title: "Chicken Biryani",
-      imgSrc: "/assets/images/ChickenBiryani.jpg",
-      price: "£19",
-      videoUrl: "/assets/vid.mp4",
-    },
-    {
-      title: "Chicken Biryani",
-      imgSrc: "/assets/images/ChickenDhabaCurry.jpg",
-      price: "£19",
-      videoUrl: "/assets/vid.mp4",
-    },
-    {
-      title: "Chicken Biryani",
-      imgSrc: "/assets/images/MuscatGosht2.jpg",
-      price: "£19",
-      videoUrl: "/assets/vid.mp4",
-    },
-    {
-      title: "Chicken Biryani",
-      imgSrc: "/assets/images/PresidentPlatter.jpg",
-      price: "£19",
-      videoUrl: "/assets/vid.mp4",
-    },
-    {
-      title: "Chicken Biryani",
-      imgSrc: "/assets/images/ChickenBiryani.jpg",
-      price: "£19",
-      videoUrl: "/assets/vid.mp4",
-    },
-    {
-      title: "Chicken Biryani",
-      imgSrc: "/assets/images/ChickenDhabaCurry.jpg",
-      price: "£19",
-      videoUrl: "/assets/vid.mp4",
-    },
-    {
-      title: "Chicken Biryani",
-      imgSrc: "/assets/images/MuscatGosht2.jpg",
-      price: "£19",
-      videoUrl: "/assets/vid.mp4",
-    },
-    {
-      title: "Chicken Biryani",
-      imgSrc: "/assets/images/PresidentPlatter.jpg",
-      price: "£19",
-      videoUrl: "/assets/vid.mp4",
-    },
-  ];
-
   return (
     <div className="z-1 flex flex-wrap sm:flex-1  w-full gap-4 mx-auto md:pl-4 pl-0 content-start  min-[960px]:justify-start md:items-start justify-center items-center py-4 h-[90vh]">
       {menuItems.map((item, index) => (
