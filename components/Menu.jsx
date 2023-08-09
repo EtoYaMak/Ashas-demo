f299368a30c60d2408d8aa522e722013eedbf26e
Mak <itsmak98@gmail.com>
2023-08-09 21:51:13 +0100

    menu j-center

--- components/Menu.jsx
+++ components/Menu.jsx
@@ -5,7 +5,7 @@ import { menuItems } from "@public/menuItems"; // adjust the path based on where
 
 const Menu = ({ onVideoSelected }) => {
   return (
-    <div className="z-1 flex flex-wrap sm:flex-1  w-full gap-4 mx-auto md:pl-4 pl-0 content-start  min-[960px]:justify-start md:items-start justify-center items-center py-4 h-[90vh]">
+    <div className="z-1 flex flex-wrap sm:flex-1  w-full gap-4 mx-auto md:pl-4 pl-0 content-start  md:items-start justify-center items-center py-4 h-[90vh]">
       {menuItems.map((item, index) => (
         <MenuItem
           key={index}
